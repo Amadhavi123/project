9d1bf0a5bb6fd1c678c0b74fd207ca9fbabf0399
Amadhavi123 <aelurimadhavi@gmail.com>
2021-03-05 16:19:39 +0530

    progress

---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+	public static void main(String [] args) {
+	int a = 45 , b = 76;
+	int sum;
+	sum=a+b;
+	System.out.println("Sum of these number is: " +sum);
+	
+	
+	}
+
+}
